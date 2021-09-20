85aac1741fb4891780b5143d6ebfbaa15d477059
Olaoluwadunni <oluwatimileyinakinpelu49@gmail.com>
2021-09-20 09:05:37 +0100

    responsive view

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+// function myFunction() {
+//     var x = document.getElementById("links");
+//     var y = document.getElementById("nav_log");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
